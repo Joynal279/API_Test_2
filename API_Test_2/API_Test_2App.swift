452b5e1e03a43be09a53f3405594ebaee639601f
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-06-25 01:01:04 +0600

    Initial Commit

---
+++ API_Test_2/API_Test_2App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Test_2App.swift
+//  API_Test_2
+//
+//  Created by MAC on 25/6/23.
+//
+
+import SwiftUI
+
+@main
+struct API_Test_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
