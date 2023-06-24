dd6021572b9a7d2d5cb390c9b9afea71425003e7
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-06-25 01:16:58 +0600

    👮‍♀️ view added

--- API_Test_2/ContentView.swift
+++ API_Test_2/ContentView.swift
@@ -5,22 +5,30 @@
 //  Created by MAC on 25/6/23.
 //
 
+import Foundation
 import SwiftUI
 
 struct ContentView: View {
+    @State private var coffee: [CoffeeModel] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Text(coffee.first?.title ?? "")
+        Button {
+            Task {
+                let (data, _) = try await URLSession.shared.data(from: URL(string:"https://devilarticle.com/api/Coffee")!)
+                let decodedResponse = try? JSONDecoder().decode([CoffeeModel].self, from: data)
+                coffee = decodedResponse ?? []
+            }
+        } label: {
+            Text("Fetch Joke")
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct CoffeeModel: Codable {
+    var id: Int
+    var title: String
 }
